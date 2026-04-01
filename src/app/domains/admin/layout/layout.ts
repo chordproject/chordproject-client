b923f2da03bc6b0893c9e111120795ded93e40f2
srcn <117369+srcn@users.noreply.github.com>
2026-04-01 14:24:39 +0300

    feat: open promotional banner link in a new tab

--- src/app/domains/admin/layout/layout.ts
+++ src/app/domains/admin/layout/layout.ts
@@ -48,6 +48,7 @@ import { AdminSidebar } from '@/app/domains/admin/layout/ui/sidebar';
           <a
             class="absolute inset-0 z-10"
             href="https://builderkit.dev?utm_source=fuse&utm_medium=banner&utm_campaign=upgrade"
+            target="_blank"
             ><span></span
           ></a>
           Check out BuilderKit; the next generation of toolkit for building
